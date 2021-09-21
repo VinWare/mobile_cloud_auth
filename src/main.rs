c4fd7c1e4829d9e485c65a666ebec94267c06917
VinWare <28603906+VinWare@users.noreply.github.com>
2021-09-21 21:18:06 +0530

    Trying out various mod options, and starting to segragate code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+// use std::env;
+mod setup;
+mod other;
+fn input_action() -> i32{
+    println!("Enter 1 for register, 2 for auth");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let n: i32 = input.trim().parse().unwrap();
+    return n
+}
+fn register() {
+    println!("register")
+}
+fn auth() {
+    println!("auth")
+}
+// fn read_env() {
+    // match env::var("ECC") {
+        // Ok(val) => println!("{}: {:?}", "ECC", val),
+        // Err(e) => println!("{}",e),
+    // };
+    // println!("{}",ecc);
+// }
 fn main() {
-    println!("Hello, world!");
+    // read_env();
+    setup::setup();
+    other::other();
+    let action = input_action();
+    // println!("{}",action);
+    match action {
+        1 => register(),
+        2 => auth(),
+        _ => {println!("error");std::process::exit(1)}
+    }
+    // println!("Hello, world!");
 }
---
+++ src/other.rs
@@ -0,0 +1,3 @@
+pub fn other() {
+    println!("other");
+}
