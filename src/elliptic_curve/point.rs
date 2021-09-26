8a35f71827a85f879dfd4ddca42f82f3be0ff072
VinWare <28603906+VinWare@users.noreply.github.com>
2021-09-26 23:31:03 +0530

    Simplified eq derive

--- src/elliptic_curve/point.rs
+++ src/elliptic_curve/point.rs
@@ -1,13 +1,13 @@
-#[derive(Copy,Clone)]
+#[derive(PartialEq, Eq, Copy,Clone)]
 pub struct Point {
     pub x: i32,
     pub y: i32,
 }
-impl PartialEq for Point {
-    fn eq(&self, other: &Self) -> bool {
-        self.x == other.x && self.y == other.y
-    }
-}
-impl Eq for Point {
-
-}
+// impl PartialEq for Point {
+    // fn eq(&self, other: &Self) -> bool {
+        // self.x == other.x && self.y == other.y
+    // }
+// }
+// impl Eq for Point {
+// 
+// }
